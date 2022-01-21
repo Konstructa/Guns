b7ab63c5ca34f43cb95b9f1be026a01ab2e675ed
Konstructa <elenprojects7@gmail.com>
2022-01-21 18:11:16 -0300

    adicionando  verificações e class service

--- src/controllers/costumer.controller.ts
+++ src/controllers/costumer.controller.ts
@@ -1,13 +1,30 @@
 import { Request, Response } from 'express';
 import bcrypt from 'bcrypt';
+import { CostumerService } from '../services/costumer.service';
 
 class CostumerController {
   static async createCostumer(req: Request, res: Response) {
     try {
-      const hashedPassword = await bcrypt.hash(req.body.password, 10);
-      return res.status(200).json({ user: req.body.user, senha: hashedPassword });
+      const { user, password } = req.body;
+
+      if (password.length === 0
+        || user.length === 0
+      ) {
+        return res.status(400)
+          .send({
+            error: 'Você não inseriu valores válidos cheque novamente!',
+          });
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+      await CostumerService.execute({ user, hashedPassword });
+
+      return res.status(200)
+        .json({
+          sucess: 'Dados registrados com sucesso!',
+        });
     } catch (e) {
-      return res.status(400).json('Erro');
+      return res.status(400).json(e);
     }
   }
 
