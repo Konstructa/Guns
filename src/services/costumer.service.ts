6047aeec5508497323ffaef5d024d7e69cce1326
Konstructa <elenprojects7@gmail.com>
2022-01-21 18:10:50 -0300

    adionando valores no banco de dados

--- src/services/costumer.service.ts
+++ src/services/costumer.service.ts
@@ -1,1 +1,24 @@
 /* import { connect } from '../database/db'; */
+import { getRepository } from 'typeorm';
+import { Costumer } from '../entities/Costumer';
+import ICostumer from '../interface/custumer';
+
+class CostumerService {
+  static async execute({ user, hashedPassword }:ICostumer) {
+    const costumers = await getRepository(Costumer)
+      .createQueryBuilder()
+      .insert()
+      .into(Costumer)
+      .values(
+        {
+          user,
+          password: hashedPassword,
+        },
+      )
+      .execute();
+    console.log(costumers);
+    return costumers;
+  }
+}
+
+export { CostumerService };
