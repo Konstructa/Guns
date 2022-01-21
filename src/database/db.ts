00903dafe5c393b4aaf0c58c5cac148674deace0
Konstructa <elenprojects7@gmail.com>
2022-01-21 00:51:19 -0300

    adicionando conexão

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,15 +1,8 @@
-import { createPool } from 'mysql2/promise';
+import { Connection, createConnection, getConnection } from 'typeorm';
 
-export async function connect() {
-  const connection = createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    connectionLimit: 10,
-  });
-
-  return connection;
-}
-
-export default connect;
+export default async (): Promise<Connection> => {
+  const defaultOption = await getConnection();
+  return createConnection(
+    Object.assign(defaultOption),
+  );
+};
