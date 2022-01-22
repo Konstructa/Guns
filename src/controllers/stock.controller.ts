66ba74c8cef7624421f689dbc901048b00990c1a
Konstructa <elenprojects7@gmail.com>
2022-01-21 22:53:28 -0300

    adicionando função

--- src/controllers/stock.controller.ts
+++ src/controllers/stock.controller.ts
@@ -4,6 +4,10 @@ class StockController {
   static async createProduct(req: Request, res: Response) {
     res.status(200).json('Rota para criar produto');
   }
+
+  static async GetProductDetailsByOrderID(req: Request, res: Response) {
+    res.status(200).json('Aqui está os detalhes do seu produto por OrderID');
+  }
 }
 
 export { StockController };
