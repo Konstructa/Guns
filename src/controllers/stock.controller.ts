bfb86fe5647b8b0f6cee73bab661c5fcbf17bd66
Konstructa <elenprojects7@gmail.com>
2022-01-22 23:31:10 -0300

    criando execute em services

--- src/controllers/stock.controller.ts
+++ src/controllers/stock.controller.ts
@@ -1,8 +1,28 @@
 import { Request, Response } from 'express';
+import { StockService } from '../services/stock.service';
 
 class StockController {
   static async createProduct(req: Request, res: Response) {
-    res.status(200).json('Rota para criar produto');
+    try {
+      const { name, quantity } = req.body;
+
+      if (quantity.length === 0
+        || name.length === 0
+      ) {
+        return res.status(400)
+          .send({
+            error: 'Você não inseriu valores válidos cheque novamente!',
+          });
+      }
+      await StockService.execute({ name, quantity });
+
+      return res.status(201)
+        .json({
+          sucess: 'Produto registrado com sucesso!',
+        });
+    } catch (e) {
+      return res.status(500).json('Erro interno');
+    }
   }
 
   static async GetProductDetailsByOrderID(req: Request, res: Response) {
