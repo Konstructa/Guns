8ce29867249d56a24d8639d61b9f456b0e270f67
Konstructa <elenprojects7@gmail.com>
2022-01-23 15:39:40 -0300

    adicinando deteteOrder

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
 import { OrderService } from '../services/order.service';
-/* import { Order } from '../entities/Order'; */
+import { StockService } from '../services/stock.service';
 import { IOrder } from '../interface/order';
 import { Costumer } from '../entities/Costumer';
 import { Product } from '../entities/Product';
+import { Order } from '../entities/Order';
 
 class OrderController {
   static async createOrder(req: Request, res: Response) {
@@ -25,41 +26,67 @@ class OrderController {
         .where('id = :id', { id: reciveOrder.costumer })
         .getOne();
 
-      const productIsAvailabe = await getRepository(Product)
-        .createQueryBuilder('navy.stock')
-        .select('quantity')
-        .where('id = :id', { id: reciveOrder.product })
-        .getRawOne();
-
-      const quantityInStock = Number(productIsAvailabe.quantity);
-
-      console.log(quantityInStock, reciveOrder.productsQuantity);
+      const quantityInStock = Number(existsProduct?.quantity);
 
       let newQuantity = 0;
 
       if (reciveOrder.productsQuantity <= quantityInStock) {
         newQuantity = quantityInStock - reciveOrder.productsQuantity;
       } else {
-        return res.status(400)
-          .json({
+        return res.status(400).json(
+          {
             error: 'Quantidade de produtos é superior ao que temos no estoque',
-          });
+          },
+        );
       }
 
       await OrderService.execute(reciveOrder.productsQuantity, existsProduct, existsCostumer);
 
-      console.log(newQuantity);
+      await StockService.update(reciveOrder.product, newQuantity);
 
-      return res.status(201).json({ sucess: 'Deu certo' });
+      return res.status(201).json(
+        {
+          sucess: 'Pedido registrado com sucesso',
+        },
+      );
     } catch (error) {
-      res.status(400).json(error);
+      res.status(500).json('Problemas de resposta no servidor');
     }
 
     return res.status(201);
   }
 
   static async deleteOneOrder(req: Request, res: Response) {
-    res.status(200).json('Rota para deletar pedidos');
+    try {
+      const orderID = req.params.id;
+
+      const id = Number.parseInt(orderID, 10);
+
+      const existsOrder = await getRepository(Order)
+        .createQueryBuilder('orders')
+        .where('id = :id', { id })
+        .getOneOrFail();
+
+      const conRepository = getRepository(Order);
+      const relation = await conRepository.findOne(
+        id,
+        { relations: ['product'] },
+      );
+
+      const productId = Number(relation?.product.id);
+
+      const productIsAvailabe = (relation?.product.quantity);
+
+      const addProductsOffDeletedOrder = Number(productIsAvailabe)
+      + Number(existsOrder.productsQuantity);
+
+      await OrderService.delete(id);
+      await StockService.update(productId, addProductsOffDeletedOrder);
+
+      res.status(200).json({ suscess: 'Pedido deletado' });
+    } catch (error) {
+      res.status(500).json({ error: 'Erro interno' });
+    }
   }
 
   static async getOneOrder(req: Request, res: Response) {
