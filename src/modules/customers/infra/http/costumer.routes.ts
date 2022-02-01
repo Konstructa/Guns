28f365c414080d9b64208d4808af0c35e47c684a
Konstructa <elenprojects7@gmail.com>
2022-02-01 19:45:48 -0300

    adicionando rota token

--- src/modules/customers/infra/http/costumer.routes.ts
+++ src/modules/customers/infra/http/costumer.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import { CustomerController } from '../../application/customer.controller';
+import { TokenController } from '../../application/token.controller';
 
 const router = Router();
 
 router.route('/')
   .post(CustomerController.createCustomer);
+
+router.route('/token')
+  .post(TokenController.store);
 
 router.route('/:id')
   .put(CustomerController.updateCustomer)
