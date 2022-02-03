e7059859f690f402af60af9d0bcb9fddfac8e13e
Konstructa <elenprojects7@gmail.com>
2022-02-03 13:54:55 -0300

    adicionando middleware nas rotas de Order

--- src/modules/orders/infra/http/orders.routes.ts
+++ src/modules/orders/infra/http/orders.routes.ts
@@ -5,15 +5,15 @@ import middleware from '../../../../shared/http/utils/middleware';
 const router = Router();
 
 router.route('/')
-  .post(OrderController.createOrder);
+  .post(middleware, OrderController.createOrder);
 
 router.route('/:id')
-  .delete(OrderController.deleteOneOrder);
+  .delete(middleware, OrderController.deleteOneOrder);
 
 router.route('/customerByOrderID/:id')
   .get(middleware, OrderController.getCustomerByOrderID);
 
 router.route('/productDetailsByOrderID/:id')
-  .get(OrderController.getProductDetailsByOrderID);
+  .get(middleware, OrderController.getProductDetailsByOrderID);
 
 export default router;
