45f2aaa4520ef1b08ce69cde6425c2973373a8ef
Konstructa <elenprojects7@gmail.com>
2022-02-03 00:13:19 -0300

    criando middleware

--- src/shared/http/utils/middleware.ts
+++ src/shared/http/utils/middleware.ts
@@ -1,5 +1,23 @@
+import jwt, { JwtPayload } from 'jsonwebtoken';
+
 import { Request, Response, NextFunction } from 'express';
 
 export default (req:Request, res: Response, next: NextFunction) => {
   const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res.status(401).json({ error: 'Login é necessário' });
+  }
+
+  const [, token] = authorization.split(' ');
+
+  try {
+    const data = jwt.verify(token, process.env.TOKEN_SECRET || '') as JwtPayload;
+    /* const { username, password } = data;
+     req.username = username;
+    req.userpassword = password; */
+    return next();
+  } catch (error) {
+    return res.status(401).json({ error: 'Token expirado ou invalido' });
+  }
 };
