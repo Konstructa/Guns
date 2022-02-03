f8e45c04b5d16be764f1fa6c0e9516608e04c2cd
Konstructa <elenprojects7@gmail.com>
2022-02-03 00:12:47 -0300

    adicionando rota de login

--- src/modules/customers/infra/http/costumer.routes.ts
+++ src/modules/customers/infra/http/costumer.routes.ts
@@ -1,17 +1,17 @@
 import { Router } from 'express';
 import { CustomerController } from '../../application/customer.controller';
-import { TokenController } from '../../application/token.controller';
+import { TokenController } from '../../application/login/login.controller';
 
 const router = Router();
 
 router.route('/')
   .post(CustomerController.createCustomer);
 
-router.route('/token')
-  .post(TokenController.store);
-
 router.route('/:id')
   .put(CustomerController.updateCustomer)
   .delete(CustomerController.deleteCustomer);
+
+router.route('/login')
+  .post(TokenController.store);
 
 export default router;
