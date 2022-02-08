67dc4fee6ab0a42c2803fa045b3b7eb4b7a79162
Konstructa <elenprojects7@gmail.com>
2022-02-08 15:22:13 -0300

    alterando path

--- src/modules/customers/infra/http/costumer.routes.ts
+++ src/modules/customers/infra/http/costumer.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { CustomerController } from '../../application/customer.controller';
-import { TokenController } from '../../application/auth/login.controller';
+import { TokenController } from '../../../auth/login.controller';
 
 const router = Router();
 
